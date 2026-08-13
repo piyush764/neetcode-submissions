d4ea9fdd20c134bb6d539cee6413260688724233
Piyush Bohra <pbohra134@gmail.com>
2026-08-13 20:44:20 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums):
+        result=[]
+        for i in nums :
+            result.append(i)
+        for i in nums :
+            result.append(i)
+        return result
+
+
+        
